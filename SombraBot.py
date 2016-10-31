abc7335ad1351496a03e5fc126cbc2875396325d
Catherine O'Neill-Lynch <catherineo@juba.local>
2016-10-31 17:33:45 +0000

    Added Coin toss, ability to show bot playing a game and edited the 8ball

--- SombraBot.py
+++ SombraBot.py
@@ -1,5 +1,7 @@
+import asyncio
 import random
 
+import discord
 from discord.ext import commands
 
 description = '''Sombra - A Simple Discord Bot'''
@@ -7,13 +9,13 @@ description = '''Sombra - A Simple Discord Bot'''
 bot = commands.Bot(command_prefix='-',
                    description=description)
 
-
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    await bot.change_presence(game=discord.Game(name='Game Title'))
 
 
 @bot.command(description='adds two numbers')
@@ -41,7 +43,26 @@ async def insult():
 @bot.command(description='8 ball')
 async def eightball():
     lines = open("data/eightball.txt").read().splitlines()
+    await bot.say('Let me look into the future')
+    await asyncio.sleep(3)
     await bot.say(random.choice(lines))
+
+
+@bot.command(description='Response to porn')
+async def porn():
+    await bot.say('Look for it yourself!')
+
+
+@bot.command(decription='A coin toss')
+async def flip():
+    outcome = random.randint(0, 1)
+    if outcome == 0:
+        outcome = "Heads"
+    else:
+        outcome = "Tails"
+    await bot.say('Flipping the coin...')
+    await asyncio.sleep(3)
+    await bot.say('The coin shows, ' + outcome)
 
 
 try:
