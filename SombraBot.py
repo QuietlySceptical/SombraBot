fb1344575514639bb846018664d36e397319cf16
Catherine O'Neill-Lynch <catherineo@juba.local>
2016-11-01 22:43:35 +0000

    Added in comptitive stats

--- SombraBot.py
+++ SombraBot.py
@@ -83,7 +83,7 @@ async def hug(member: discord.Member):
 
 
 @bot.command(description='Average player stats for Overwatch user')
-async def owquickplay(username: str):
+async def owquick(username: str):
     api_url = 'https://owapi.net/api/v3/u/{0}/stats'
     battlenet = username.replace('#', '-')
     headers = {'User-agent': 'SombraBot'}
@@ -106,7 +106,7 @@ async def owquickplay(username: str):
     time_spent_on_fire_avg = data['eu']['stats']['quickplay']['average_stats']['time_spent_on_fire_avg']
 
     await bot.say('```Quick Play stats for {0}\n'
-                  'Presitige: {1}\n'
+                  'Prestige: {1}\n'
                   'Level: {2}\n'
                   'Time Played: {3}\n'
                   'Wins: {4}\n\n'
@@ -120,6 +120,50 @@ async def owquickplay(username: str):
                   'Avg Time on Fire: {12}\n'
                   '```'
                   .format(username, prestige, level, time_played, wins, eliminations_avg,
+                          killperdeath, healing_done_avg, offensive_assists_avg, defensive_assists_avg,
+                          damage_done_avg, deaths_avg, time_spent_on_fire_avg))
+
+
+@bot.command(description='Average player stats for Overwatch user')
+async def owcomp(username: str):
+    api_url = 'https://owapi.net/api/v3/u/{0}/stats'
+    battlenet = username.replace('#', '-')
+    headers = {'User-agent': 'SombraBot'}
+
+    status = requests.get(api_url.format(battlenet), headers=headers)
+    data = status.json()
+    print(status)
+
+    comprank = data['eu']['stats']['competitive']['overall_stats']['comprank']
+    games_played = data['eu']['stats']['competitive']['game_stats']['games_played']
+    win_rate = data['eu']['stats']['competitive']['overall_stats']['win_rate']
+    wins = data['eu']['stats']['competitive']['overall_stats']['wins']
+    losses = data['eu']['stats']['competitive']['overall_stats']['losses']
+    eliminations_avg = data['eu']['stats']['competitive']['average_stats']['eliminations_avg']
+    killperdeath = data['eu']['stats']['competitive']['game_stats']['kpd']
+    healing_done_avg = data['eu']['stats']['competitive']['average_stats']['healing_done_avg']
+    offensive_assists_avg = data['eu']['stats']['competitive']['average_stats']['offensive_assists_avg']
+    defensive_assists_avg = data['eu']['stats']['competitive']['average_stats']['defensive_assists_avg']
+    damage_done_avg = data['eu']['stats']['competitive']['average_stats']['damage_done_avg']
+    deaths_avg = data['eu']['stats']['competitive']['average_stats']['deaths_avg']
+    time_spent_on_fire_avg = data['eu']['stats']['competitive']['average_stats']['time_spent_on_fire_avg']
+
+    await bot.say('```Competitive stats for {0}\n'
+                  'Rank: {1}\n'
+                  'Games Played: {2}\n'
+                  'Win Rate: {3}\n'
+                  'Wins: {4}\n'
+                  'Losses: {5}\n\n'
+                  'Avg Eliminations: {6}\n'
+                  'KPD: {7}\n'
+                  'Avg Healing Done: {8}\n'
+                  'Avg Offensive Assists: {9}\n'
+                  'Avg Defensive Assists: {10}\n'
+                  'Avg Damage Done: {11}\n'
+                  'Avg Deaths: {12}\n'
+                  'Avg Time on Fire: {13}\n'
+                  '```'
+                  .format(username, comprank, games_played, win_rate, wins, losses, eliminations_avg,
                           killperdeath, healing_done_avg, offensive_assists_avg, defensive_assists_avg,
                           damage_done_avg, deaths_avg, time_spent_on_fire_avg))
 
