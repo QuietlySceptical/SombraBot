e1928c73d788fd3d875095ca5602337f4052cb6e
QuietlySceptical <coneilllynch01@qub.ac.uk>
2016-11-03 15:28:32 +0000

    Separated the imgur command into multiple commands

--- SombraBot.py
+++ SombraBot.py
@@ -73,21 +73,21 @@ async def choose(*choices: str):
 @bot.command()
 async def compliment():
     """Gives a random compliment"""
-    lines = open("data/compliment.txt").read().splitlines()
+    lines = open('data/compliment.txt').read().splitlines()
     await  bot.say(random.choice(lines))
 
 
 @bot.command()
 async def insult():
     """Gives a random insult"""
-    lines = open("data/insult.txt").read().splitlines()
+    lines = open('data/insult.txt').read().splitlines()
     await  bot.say(random.choice(lines))
 
 
 @bot.command()
 async def eightball(*message: str):
     """Ask a question and the EightBall will give an answer"""
-    lines = open("data/eightball.txt").read().splitlines()
+    lines = open('data/eightball.txt').read().splitlines()
     await bot.say('So you want to know: ' + ' ' .join(map(str, message)) + '\n ...Let me look into the future')
     await asyncio.sleep(3)
     await bot.say(random.choice(lines))
@@ -103,9 +103,9 @@ async def flip():
     """Flips a coin, resulting in Heads or Tails"""
     outcome = random.randint(0, 1)
     if outcome == 0:
-        outcome = "Heads"
+        outcome = 'Heads'
     else:
-        outcome = "Tails"
+        outcome = 'Tails'
     await bot.say('Flipping the coin...')
     await asyncio.sleep(3)
     await bot.say('The coin shows, ' + outcome)
@@ -235,35 +235,51 @@ async def penguin():
 
 
 @bot.command()
-async def imgur(*text: str):
-    if text == ():
-        rand = random.randint(0, 59)  # 60 results generated per page
-        items = imgurclient.gallery_random(page=0)
-        await bot.say('Random image')
-        await bot.say(items[rand].link)
+async def imgrand():
+    """Shows the user a random image from imgur"""
+    rand = random.randint(0, 59)  # 60 results generated per page
+    items = imgurclient.gallery_random(page=0)
+    await bot.say(items[rand].link)
 
-    elif text[0] == "search":
-        items = imgurclient.gallery_search(" ".join(text[1:len(text)]), advanced=None, sort='time', page=0)
+
+@bot.command()
+async def imgsearch(*text: str):
+    """Allows the user to search for an image from imgur"""
+    rand = random.randint(0, 59)
+    if text == ():
+        await bot.say('Please enter a search term')
+    elif text[0] != ():
+        items = imgurclient.gallery_search(" ".join(text[0:len(text)]), advanced=None, sort='viral', window='all', page=0)
         if len(items) < 1:
-            await bot.say("Your search gave no results")
+            await bot.say('Your search gave no results')
+        else:
+            await bot.say(items[rand].link)
+
+
+@bot.command()
+async def imgtop(*text: str):
+    """Shows the top image from a specified subrreddit"""
+    if text == ():
+        await bot.say('Please enter a subbreddit')
+    elif text[0] != ():
+        items = imgurclient.subreddit_gallery(" ".join(text[0:len(text)]), sort='top', window='day', page=0)
+        if len(items) < 1:
+            await bot.say('This subreddit section does not exist, try funny')
         else:
             await bot.say(items[0].link)
+
+
+@bot.command()
+async def imgnew(*text: str):
+    """Shows the newest image from a specified subreddit"""
+    if text == ():
+        await bot.say('Please enter a subbreddit')
     elif text[0] != ():
-        try:
-            if text[1] == 'top':
-                imgsort = 'top'
-            elif text[1] == 'new':
-                imgsort = 'time'
-            else:
-                await bot.say('Only top or new is a valid subcommand')
-                return
-            items = imgurclient.subreddit_gallery(text[0], sort=imgsort, window='day', page=0)
-            if len(imgsort) < 3:
-                await bot.say('This subreddit section does not exist, try funny')
-            else:
-                await bot.say('{} {} {}'.format(items[0].link, items[1].link, items[2].link))
-        except:
-            await bot.say('Inform Caffy of the error')
+        items = imgurclient.subreddit_gallery(" ".join(text[0:len(text)]), sort='time', window='day', page=0)
+        if len(items) < 1:
+            await bot.say('This subreddit section does not exist, try funny')
+        else:
+            await bot.say(items[0].link)
 
 
 async def my_background_task():
@@ -271,7 +287,7 @@ async def my_background_task():
     counter = 0
     while not client.is_closed:
         counter += 1
-        lines = open("data/playing.txt").read().splitlines()
+        lines = open('data/playing.txt').read().splitlines()
         playing = random.choice(lines)
         await bot.change_presence(game=discord.Game(name=playing))
         await asyncio.sleep(120)  # task runs every 120 seconds
@@ -280,4 +296,4 @@ try:
     bot.loop.create_task(my_background_task())
     bot.run('token')
 except KeyError:
-    print("Environment variable not found.")
+    print('Environment variable not found.')
