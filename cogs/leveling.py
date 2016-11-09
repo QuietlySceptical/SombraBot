641ff09a2fb41356b7b4f433ffdb49d20563f380
Padraig Donnelly <pdonnelly19@qub.ac.uk>
2016-11-09 13:39:21 +0000

    Fixed a bug that was causing the bot not to recognise cog commands.  See diff for changes.

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -3,23 +3,22 @@ from discord.ext import commands
 
 log = logging.getLogger(__name__)
 
+
 class Level:
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(pass_context=True)
+    async def rank(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await self.bot.say('Incorrect subcommand passed.')
 
-@commands.group(pass_context=True)
-async def rank(self, ctx):
-    if ctx.invoked_subcommand is None:
-        await self.bot.say('Incorrect subcommand passed.')
-
-
-@rank.command()
-async def join(bot):
-    """Join the ranking system"""
-    await bot.send_message('join')
+    @commands.command(pass_context=True)
+    async def join(self, bot):
+        """Join the ranking system"""
+        await self.bot.say('join')
 
 
-def setup(self):
-    self.add_cog(Level(self))
+def setup(bot):
+    bot.add_cog(Level(bot))
     log.info('Levelling system set up complete.')
